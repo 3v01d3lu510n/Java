b4ca1291dda7ac4f3242f0037629cd3e333224e3
3v01d3lu510n <112835113+3v01d3lu510n@users.noreply.github.com>
2024-03-06 11:33:01 +0700

    Add files via upload

---
+++ LAB211/J1.S.P0056/src/j1/s/p0056/J1SP0056.java
@@ -0,0 +1,16 @@
+package j1.s.p0056;
+
+/**
+ *
+ * @author VPC
+ */
+public class J1SP0056 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
